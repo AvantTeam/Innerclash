fbf2fef5486b8b9bbac89a50314f83e0bf731903
JerichoFletcher <jericho.russel17@gmail.com>
2021-05-13 12:45:00 +0700

    fixed: jumps registered too quickly

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 namespace Innerclash.Entities {
     public class PlayerController : EntityController {
-        public float maxJumpBufferTime = 0.1f, maxCoyoteTime = 0.04f;
+        public float minJumpDelay = 0.2f, maxJumpBufferTime = 0.1f, maxCoyoteTime = 0.04f;
 
-        private float jumpBufferTimer = -Mathf.Infinity, coyoteTimer = 0;
+        private float jumpTimer = 0f, jumpBufferTimer = -Mathf.Infinity, coyoteTimer = 0f;
         private bool hasJumped;
 
         private void Update() {
@@ -17,21 +17,24 @@ namespace Innerclash.Entities {
                 if (controllable.Grounded) {
                     coyoteTimer = Time.time;
                     if(!hasJumped && Time.time < jumpBufferTimer + maxJumpBufferTime) {
-                        controllable.Jump();
-                        jumpBufferTimer = 0f;
-                        hasJumped = true;
+                        DoJump();
                     }
                 }
                 if (jump) {
                     jumpBufferTimer = Time.time;
                     if(!hasJumped && Time.time < coyoteTimer + maxCoyoteTime) {
-                        controllable.Jump();
-                        coyoteTimer = 0f;
-                        hasJumped = true;
+                        DoJump();
                     }
                 }
-                if (Input.GetButtonUp("Jump")) hasJumped = false;
+                if (Time.time > jumpTimer + minJumpDelay) hasJumped = false;
             }
+        }
+
+        private void DoJump() {
+            controllable.Jump();
+            coyoteTimer = 0f;
+            hasJumped = true;
+            jumpTimer = Time.time;
         }
     }
 }

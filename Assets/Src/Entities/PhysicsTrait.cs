b0fcee0c75a4aba5f4a17cab6e6bc8e884654773
GlennFolker <glenn.folk08@gmail.com>
2021-07-26 15:30:16 +0700

    avoid auto jump, jump buffer time

--- Assets/Src/Entities/PhysicsTrait.cs
+++ Assets/Src/Entities/PhysicsTrait.cs
@@ -9,25 +9,32 @@ namespace Innerclash.Entities {
         public GroundCheck ground;
         /// <summary> Force used to move </summary>
         public float moveForce = 2500f;
-        /// <summary> The velocity length at which this entity won't add a move force if the value is >= to this </summary>
+        /// <summary> The velocity length at which this entity will reduce the move force if the value is >= to this </summary>
         public float maxSpeed = 15f;
         /// <summary> The force used when jumping on ground </summary>
-        public float jumpForce = 360f;
+        public float jumpForce = 640f;
         /// <summary> The force used to continue the jump mid-air </summary>
-        public float jumpContinuousForce = 2400f;
+        public float jumpContinuousForce = 2500f;
         /// <summary> The maximum duration of jump continuation in seconds </summary>
         public float jumpDuration = 0.5f;
+        /// <summary> The entity can regain full jump impulse after waiting for this amount of time after last jump, in seconds </summary>
+        public float jumpCooldown = 0.3f;
+        /// <summary> The entity would still be able to jump when it hits the ground if the jump trigger was within this amount of time, in seconds </summary>
+        public float jumpBufferTime = 0.15f;
 
         int hitCount;
         RaycastHit2D[] hits;
 
-        bool jumped = false;
+        bool impulseJump;
+        bool wasJumping;
+        float lastJumped;
+        float lastJumpedMidAir;
 
         public Rigidbody2D Body { get; private set; }
         public Vector2 MoveAxis { get; private set; }
 
         /// <summary> Whether its #ground check is colliding with a tile </summary>
-        public bool IsGrounded { get => hitCount > 0; }
+        public bool IsGrounded { get; private set; }
         /// <summary> Whether it has a movement target direction </summary>
         public bool IsMoving { get => MoveAxis.magnitude > 0.1f; }
         /// <summary> Whether its movement target direction clashes with the velocity's direction </summary>
@@ -55,15 +62,21 @@ namespace Innerclash.Entities {
         }
 
         void FixedUpdate() {
+            Vector2 pos = transform.position;
+
             var axis = new Vector2(MoveAxis.x, 0f);
             var velX = new Vector2(Body.velocity.x, 0f);
             if(velX.magnitude < maxSpeed || IsTurning) {
-                Body.AddForce(moveForce * Time.fixedDeltaTime * axis, ForceMode2D.Force);
+                Body.AddForceAtPosition(
+                    moveForce * Time.fixedDeltaTime * axis,
+                    pos + ground.Center,
+                    ForceMode2D.Force
+                );
             }
 
             if(hits != null) {
                 hitCount = Physics2D.RaycastNonAlloc(
-                    (Vector2)transform.position + new Vector2(ground.offsetX - ground.width / 2f, ground.offsetY - 0.01f),
+                    pos + new Vector2(ground.offsetX - ground.width / 2f, ground.offsetY - 0.01f),
                     Vector2.right,
                     hits,
                     ground.width,
@@ -73,14 +86,27 @@ namespace Innerclash.Entities {
                 for(int i = 0; i < hitCount; i++) {
                     Tilemaps.ApplyTile(hits[i].point, this);
                 }
+
+                IsGrounded = hitCount > 0;
             }
 
-            if(jumped) {
-                jumped = false;
+            if(impulseJump) {
+                impulseJump = false;
+                wasJumping = false;
+
                 IsJumping = true;
                 JumpTime = 0f;
 
-                Body.AddForce(new Vector2(0f, jumpForce) * Time.fixedDeltaTime, ForceMode2D.Impulse);
+                float alpha = Mathf.Min(Time.time - lastJumped, jumpCooldown) / jumpCooldown;
+                Debug.Log(alpha);
+                Body.AddForceAtPosition(
+                    Time.fixedDeltaTime * new Vector2(0f, jumpForce * alpha),
+                    pos + ground.Center,
+                    ForceMode2D.Impulse
+                );
+
+                IsGrounded = false;
+                lastJumped = Time.time;
             } else if(IsJumping) {
                 JumpTime += Time.fixedDeltaTime;
 
@@ -89,7 +115,11 @@ namespace Innerclash.Entities {
                     JumpTime = 0f;
                 } else {
                     float alpha = 1f - JumpTime / jumpDuration;
-                    Body.AddForce(alpha * new Vector2(0f, jumpContinuousForce) * Time.fixedDeltaTime, ForceMode2D.Force);
+                    Body.AddForceAtPosition(
+                        Time.fixedDeltaTime * new Vector2(0f, jumpContinuousForce * alpha),
+                        pos + ground.Center,
+                        ForceMode2D.Force
+                    );
                 }
             } else {
                 JumpTime = 0f;
@@ -99,10 +129,19 @@ namespace Innerclash.Entities {
         public void OnMove(Vector2 axis) => MoveAxis = axis;
 
         public void OnJump(bool jump) {
-            if(IsGrounded) {
-                jumped = jump;
-            } else if(IsJumping && JumpTime < jumpDuration) {
-                IsJumping = jump;
+            if(!IsGrounded) {
+                if(IsJumping && JumpTime < jumpDuration) {
+                    IsJumping = jump;
+                }
+
+                if(!wasJumping) lastJumpedMidAir = Time.time;
+                wasJumping = jump;
+            } else if(IsGrounded && !IsJumping) {
+                if(!wasJumping || Time.time - lastJumpedMidAir <= jumpBufferTime) {
+                    impulseJump = jump;
+                } else {
+                    wasJumping = jump;
+                }
             }
         }
 
@@ -118,6 +157,7 @@ namespace Innerclash.Entities {
 
             public Vector2 LeftLimit { get => new Vector2(offsetX - width / 2f, offsetY); }
             public Vector2 RightLimit { get => new Vector2(offsetX + width / 2f, offsetY); }
+            public Vector2 Center { get => new Vector2(offsetX, offsetY); }
         }
     }
 }

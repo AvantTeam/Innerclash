d4ec1c667600fd90f6a657e4a81e34dc94faad62
GlennFolker <glenn.folk08@gmail.com>
2021-07-27 21:11:36 +0700

    merge game controller and player

--- Assets/Src/Core/GameController.cs
+++ Assets/Src/Core/GameController.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
+using Innerclash.Entities;
 using Innerclash.UI.Fragments.Overview;
+using Innerclash.Utils;
 
 using static UnityEngine.InputSystem.InputAction;
 using static Innerclash.Misc.Item;
@@ -12,7 +14,6 @@ namespace Innerclash.Core {
         [Header("Constant components")]
         public Tilemap tilemap;
         public Camera mainCamera;
-        public Player player;
 
         [Header("Default Prefabs")]
         public GameObject itemEntity;
@@ -22,6 +23,11 @@ namespace Innerclash.Core {
         public GameObject hoverPanel;
         public GameObject overviewFragment;
         public InventoryFragment inventoryFragment;
+        public ControllableTrait controlled;
+
+        Vector2 moveAxis;
+        bool jump;
+        float breakPress, actPress;
 
         public ItemStack CurrentStack { get; set; }
         public bool HoldingStack { get => CurrentStack.item != null && CurrentStack.amount > 0; }
@@ -54,11 +60,26 @@ namespace Innerclash.Core {
                 image.enabled = false;
                 text.enabled = false;
             }
+
+            breakPress = ViewingOverview ? 0f : breakPress;
+            actPress = ViewingOverview ? 0f : actPress;
+
+            if(breakPress > 0f) {
+                Tilemaps.RemoveTile(mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+            }
+            if(actPress > 0f) {
+
+            }
         }
 
         void FixedUpdate() {
-            var pos = player.controlled.transform.position;
+            var pos = controlled.transform.position;
             mainCamera.transform.position = new Vector3(pos.x, pos.y, -10f);
+
+            controlled.OnMove(moveAxis);
+            controlled.OnJump(jump);
+            controlled.OnBreak(breakPress);
+            controlled.OnAct(actPress);
         }
 
         public void OnOpenOverview(CallbackContext context) {
@@ -68,6 +89,32 @@ namespace Innerclash.Core {
                 overviewFragment.SetActive(v);
                 inventoryFragment.OnVisibilityChange(v);
             }
+        }
+
+        /// <summary>
+        /// Keyboard: W-A-S-D
+        /// </summary>
+        public void OnMove(CallbackContext context) => moveAxis = context.ReadValue<Vector2>();
+
+        /// <summary>
+        /// Keyboard: Spacebar
+        /// </summary>
+        public void OnJump(CallbackContext context) => jump = context.performed;
+
+        /// <summary>
+        /// Mouse: Left click
+        /// </summary>
+        public void OnBreak(CallbackContext context) {
+            float val = context.ReadValue<float>();
+            breakPress = val >= InputSystem.settings.defaultButtonPressPoint ? val : 0f;
+        }
+
+        /// <summary>
+        /// Mouse: Right click
+        /// </summary>
+        public void OnAct(CallbackContext context) {
+            float val = context.ReadValue<float>();
+            actPress = val >= InputSystem.settings.defaultButtonPressPoint ? val : 0f;
         }
     }
 }
--- Assets/Src/Entities/ControllableTrait.cs
+++ Assets/Src/Entities/ControllableTrait.cs
@@ -5,9 +5,15 @@ namespace Innerclash.Entities {
     public class ControllableTrait : MonoBehaviour {
         public event Action<Vector2> Move;
         public event Action<bool> Jump;
+        public event Action<float> Break;
+        public event Action<float> Act;
 
         public void OnMove(Vector2 axis) => Move?.Invoke(axis);
 
         public void OnJump(bool jump) => Jump?.Invoke(jump);
+
+        public void OnBreak(float press) => Break?.Invoke(press);
+
+        public void OnAct(float press) => Act?.Invoke(press);
     }
 }

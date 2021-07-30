6897f2ba9020b889fb54d458402fc1faeb666e08
GlennFolker <glenn.folk08@gmail.com>
2021-07-30 17:13:13 +0700

    death is fun

--- Assets/Src/Core/LightManager.cs
+++ Assets/Src/Core/LightManager.cs
@@ -1,13 +1,11 @@
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using Innerclash.Utils;
 
 namespace Innerclash.Core {
     [RequireComponent(typeof(SpriteRenderer))]
     public class LightManager : MonoBehaviour {
-        public Material shaderMaterial;
-
-        public int pixelsPerUnit;
         public int extension;
         public int padding;
 
@@ -15,8 +13,9 @@ namespace Innerclash.Core {
         public Camera Cam { get => GameController.Instance.mainCamera; }
 
         Texture2D texture;
+
         Color[] colors;
-        bool[] emits;
+        readonly List<Vector2Int> emits = new List<Vector2Int>();
 
         static int radius = 8;
         static float normalDropoff = 0.7f;
@@ -33,77 +32,75 @@ namespace Innerclash.Core {
             RecalculatePosition();
         }
 
-        void FixedUpdate() {
-            if((Cam.transform.position - Renderer.transform.position).magnitude >= extension - padding) {
-                RecalculatePosition();
-            }
-
+        void Update() {
+            RecalculatePosition();
             RecalculateColors();
         }
 
         void RecalculatePosition() {
-            Renderer.transform.position = new Vector2(
+            Vector2 previous = transform.position;
+            transform.position = new Vector2(
                 Mathf.RoundToInt(Cam.transform.position.x / extension) * extension + 0.5f,
                 Mathf.RoundToInt(Cam.transform.position.y / extension) * extension
             );
 
-            int height = (int)(Cam.orthographicSize * 2 * pixelsPerUnit);
+            if(previous != (Vector2)transform.position) {
+                RecalculateColors();
+            }
+
+            int height = (int)(Cam.orthographicSize * 2);
             int width = height * Screen.width / Screen.height;
-            height += extension * pixelsPerUnit * 2;
-            width += extension * pixelsPerUnit * 2;
+            height += extension * 2;
+            width += extension * 2;
 
             if(texture == null || (texture.width != width || texture.height != height)) {
                 texture = new Texture2D(width, height);
                 colors = new Color[width * height];
-                emits = new bool[width * height];
 
-                Renderer.sprite = Sprite.Create(texture, new Rect(0, 0, width, height), Vector2.one * 0.5f, pixelsPerUnit);
+                Renderer.sprite = Sprite.Create(texture, new Rect(0, 0, width, height), Vector2.one * 0.5f, 1);
             }
         }
 
         void RecalculateColors() {
-            Vector2 pos = Renderer.transform.position;
+            if(texture == null) return;
+
+            Vector2 pos = transform.position;
             int width = texture.width,
                 height = texture.height;
 
-            for(int x = 0; x < width; x++) {
-                for(int y = 0; y < height; y++) {
+            emits.Clear();
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
                     int idx = x + y * width;
 
                     var tile = Tilemaps.GetTile(pos + new Vector2(x - width / 2, y - height / 2));
                     if(tile == null || tile.emitsLight) {
-                        colors[idx] = tile == null ? new Color(1f, 1f, 1f, 1f) : tile.emitLight;
-                        emits[idx] = true;
+                        var color = tile == null ? Color.white : tile.emitLight;
+                        colors[idx] = color;
+
+                        emits.Add(new Vector2Int(x, y));
                     } else {
-                        colors[idx] = new Color(0f, 0f, 0f, 1f);
-                        emits[idx] = false;
+                        colors[idx] = Color.black;
                     }
                 }
             }
 
-            for(int x = 0; x < width; x++) {
-                for(int y = 0; y < height; y++) {
-                    int idx = x + y * width;
-
-                    if(emits[idx]) Emit(idx);
-                }
+            foreach(var emit in emits) {
+                Emit(emit.x, emit.y);
             }
 
             texture.SetPixels(colors);
             texture.Apply();
         }
 
-        void Emit(int idx) {
-            int rootX = idx % texture.width,
-                rootY = idx / texture.width;
-
+        void Emit(int rootX, int rootY) {
             for(int i = 0; i < singleEmission.Length; i++) {
                 singleEmission[i] = new Color();
             }
 
             fillQueue.Clear();
 
-            singleEmission[radius + radius * diameter] = colors[idx];
+            singleEmission[radius + radius * diameter] = colors[rootX + rootY * texture.width];
             fillQueue.Add(new Vector2Int(rootX, rootY));
 
             while(fillQueue.Count > 0) {
@@ -117,7 +114,7 @@ namespace Innerclash.Core {
 
                 bool pass = false;
                 var currentColor = colors[index];
-                var targetColor = singleEmission[radius + x - rootX + (radius * y - rootY) * diameter];
+                var targetColor = singleEmission[(radius + x - rootX) + (radius + y - rootY) * diameter];
 
                 if(
                     (targetColor.r > threshold || targetColor.g > threshold || targetColor.b > threshold) &&
@@ -133,22 +130,21 @@ namespace Innerclash.Core {
 
                 if(!(x == rootX && y == rootY) && !pass) continue;
 
-                for(int nx = x - 1; nx <= x + 1; nx++) {
-                    for(int ny = y - 1; ny <= y + 1; ny++) {
+                for(int ny = y - 1; ny <= y + 1; ny++) {
+                    for(int nx = x - 1; nx <= x + 1; nx++) {
                         int neighbor = Mathf.Max(Mathf.Abs(nx - rootX), Mathf.Abs(ny - rootY));
                         if(neighbor <= radius && neighbor == currentLayer + 1) {
                             float dropoff = (nx != x && ny != y) ? diagonalDropoff : normalDropoff;
-                            int emitX = radius + nx - rootX,
-                                emitY = radius + ny - rootY;
+                            int eidx = (radius + nx - rootX) + (radius + ny - rootY) * diameter;
 
-                            var emit = singleEmission[emitX + emitY * diameter];
-                            if(emit.r + emit.g + emit.b == 0f) {
+                            var emit = singleEmission[eidx];
+                            if(Structs.InBounds(colors, nx + ny * texture.width) && emit.r + emit.g + emit.b == 0f) {
                                 fillQueue.Add(new Vector2Int(nx, ny));
                             }
 
-                            singleEmission[emitX + emitY * diameter].r = Mathf.Max(targetColor.r * dropoff, singleEmission[emitX + emitY * diameter].r);
-                            singleEmission[emitX + emitY * diameter].g = Mathf.Max(targetColor.g * dropoff, singleEmission[emitX + emitY * diameter].g);
-                            singleEmission[emitX + emitY * diameter].b = Mathf.Max(targetColor.b * dropoff, singleEmission[emitX + emitY * diameter].b);
+                            singleEmission[eidx].r = Mathf.Max(targetColor.r * dropoff, singleEmission[eidx].r);
+                            singleEmission[eidx].g = Mathf.Max(targetColor.g * dropoff, singleEmission[eidx].g);
+                            singleEmission[eidx].b = Mathf.Max(targetColor.b * dropoff, singleEmission[eidx].b);
                         }
                     }
                 }
